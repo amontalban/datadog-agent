9095d085216acc3125852089ab0f1276ca8c2b71
Andres Montalban <amontalban@perceptyx.com>
2019-07-10 12:49:12 -0300

    Fix compilation under FreeBSD

--- rtloader/rtloader/api.cpp
+++ rtloader/rtloader/api.cpp
@@ -24,6 +24,9 @@
 #elif _WIN32
 #    define DATADOG_AGENT_TWO "libdatadog-agent-two.dll"
 #    define DATADOG_AGENT_THREE "libdatadog-agent-three.dll"
+#elif __FreeBSD__
+#    define DATADOG_AGENT_TWO "libdatadog-agent-two.so"
+#    define DATADOG_AGENT_THREE "libdatadog-agent-three.so"
 #else
 #    error Platform not supported
 #endif
